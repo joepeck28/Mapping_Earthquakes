0970a8259b4aa5bda4d0ef071874ce31dd2a11a7
joepeck28 <110148559+joepeck28@users.noreply.github.com>
2022-12-15 13:13:18 -0800

    Adding Layer Data

--- Earthquakes_past7days/static/js/logicstep3.js
+++ Earthquakes_past7days/static/js/logicstep3.js
@@ -46,7 +46,7 @@ d3.json(earthquakeData).then(function(data) {
     return {
       opacity: 1,
       fillOpacity: 1,
-      fillColor: "#ffae42",
+      fillColor: getColor(feature.properties.mag),
       color: "#000000",
       radius: getRadius(feature.properties.mag),
       stroke: true,
@@ -63,16 +63,39 @@ d3.json(earthquakeData).then(function(data) {
   return magnitude * 4;
 }
 
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+}
+
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJSON(data, {
-
   // We turn each feature into a circleMarker on the map.
-  
   pointToLayer: function(feature, latlng) {
-              console.log(data);
-              return L.circleMarker(latlng);
-          },
-        // We set the style for each circleMarker using our styleInfo function.
-      style: styleInfo
-      }).addTo(map);
+      console.log(data);
+      return L.circleMarker(latlng);
+    },
+  // We set the style for each circleMarker using our styleInfo function.
+style: styleInfo,
+  // We create a popup for each circleMarker to display the magnitude and
+  //  location of the earthquake after the marker has been created and styled.
+  onEachFeature: function(feature, layer) {
+  layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+}
+}).addTo(map);
   });
